a3b932581d52344981324ffddff880c152abcbfb
gin337 <steamgregor9@gmail.com>
2023-09-02 15:20:04 +0200

    d

--- bukkit-core-api/src/main/java/de/teamholy/core/bukkit/manager/CustomBannerManager.java
+++ bukkit-core-api/src/main/java/de/teamholy/core/bukkit/manager/CustomBannerManager.java
@@ -2,23 +2,19 @@ package de.teamholy.core.bukkit.manager;
 
 import de.teamholy.core.bukkit.BukkitCore;
 import org.bukkit.DyeColor;
-import org.bukkit.Location;
+
 import org.bukkit.Material;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
+
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class CustomBannerManager {
 
     private BukkitCore bukkitCore;
-
-    private Map<Player, Boolean> playerBannerMap = new HashMap<>();
 
     public CustomBannerManager(BukkitCore bukkitCore) {
         this.bukkitCore = bukkitCore;
@@ -39,7 +35,6 @@ public class CustomBannerManager {
                     }
 
                     player.getInventory().setHelmet(banner);
-                    playerBannerMap.put(player, true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -54,15 +49,12 @@ public class CustomBannerManager {
     }
 
     public void removeCustomBanner(Player player) {
-        if (playerBannerMap.containsKey(player) && playerBannerMap.get(player)) {
-            ItemStack currentHelmet = player.getInventory().getHelmet();
+        ItemStack currentHelmet = player.getInventory().getHelmet();
             if (currentHelmet != null && currentHelmet.getType() == Material.BANNER) {
                 player.getInventory().setHelmet(null);
-                playerBannerMap.remove(player);
-                System.out.println("removed custom banner");
+
             }
-        }
-    }
+     }
 
 
 
