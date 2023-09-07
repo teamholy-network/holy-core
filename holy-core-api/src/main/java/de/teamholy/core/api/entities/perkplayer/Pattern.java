d5c83309fc4fe92733952c26bc4812771befed7c
yassino <letzyassinehd@gmail.com>
2023-09-07 21:12:03 +0200

    pattern class

---
+++ holy-core-api/src/main/java/de/teamholy/core/api/entities/perkplayer/Pattern.java
@@ -0,0 +1,13 @@
+package de.teamholy.core.api.entities.perkplayer;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@Setter
+public class Pattern {
+    private String color;
+    private String patternName;
+}
