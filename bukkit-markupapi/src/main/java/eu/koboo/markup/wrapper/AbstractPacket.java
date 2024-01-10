81d0029ea8342a3737843d8df1a2e3d5ba553217
charonxyz <68405438+charonxyz@users.noreply.github.com>
2024-01-10 16:22:49 +0100

    Added Getter to AbstractPacket

--- bukkit-markupapi/src/main/java/eu/koboo/markup/wrapper/AbstractPacket.java
+++ bukkit-markupapi/src/main/java/eu/koboo/markup/wrapper/AbstractPacket.java
@@ -22,10 +22,12 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
 import com.google.common.base.Objects;
+import lombok.Getter;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
 
+@Getter
 public abstract class AbstractPacket {
     // The packet we will be modifying
     protected PacketContainer handle;
@@ -42,18 +44,9 @@ public abstract class AbstractPacket {
             throw new IllegalArgumentException("Packet handle cannot be NULL.");
         if (!Objects.equal(handle.getType(), type))
             throw new IllegalArgumentException(handle.getHandle()
-                    + " is not a packet of type " + type);
+                + " is not a packet of type " + type);
 
         this.handle = handle;
-    }
-
-    /**
-     * Retrieve a handle to the raw packet data.
-     *
-     * @return Raw packet data.
-     */
-    public PacketContainer getHandle() {
-        return handle;
     }
 
     /**
@@ -65,7 +58,7 @@ public abstract class AbstractPacket {
     public void sendPacket(Player receiver) {
         try {
             ProtocolLibrary.getProtocolManager().sendServerPacket(receiver,
-                    getHandle());
+                getHandle());
         } catch (InvocationTargetException e) {
             throw new RuntimeException("Cannot send packet.", e);
         }
@@ -90,7 +83,7 @@ public abstract class AbstractPacket {
     public void recievePacket(Player sender) {
         try {
             ProtocolLibrary.getProtocolManager().recieveClientPacket(sender,
-                    getHandle());
+                getHandle());
         } catch (Exception e) {
             throw new RuntimeException("Cannot receive packet.", e);
         }
@@ -105,7 +98,7 @@ public abstract class AbstractPacket {
     public void receivePacket(Player sender) {
         try {
             ProtocolLibrary.getProtocolManager().recieveClientPacket(sender,
-                    getHandle());
+                getHandle());
         } catch (Exception e) {
             throw new RuntimeException("Cannot receive packet.", e);
         }
