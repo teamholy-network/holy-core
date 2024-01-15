cf4421610115a9a39eb3405127d89e51d2acd838
gin337 <steamgregor9@gmail.com>
2024-01-15 21:09:21 +0100

    Revert "Title fix, added back mitigate threshold"
    This reverts commit b1c07f487cee97054ca0714f75a2ef045c5011c7.

--- bungee-login/src/main/java/de/teamholy/bungee/login/manager/BotManager.java
+++ bungee-login/src/main/java/de/teamholy/bungee/login/manager/BotManager.java
@@ -18,13 +18,13 @@ public class BotManager {
 	}
 
 	public static boolean isBotProtectionEnableRegister() {
-		//return true;
-        return registers >= 5;
+		return true;
+        //registers >= 5;
 	}
 
 	public static boolean isBotProtectionEnableLogin() {
-		//return true;
-        return loggins >= 10;
+		return true;
+        //loggins >= 10;
 	}
 	
 	public static void register() {
