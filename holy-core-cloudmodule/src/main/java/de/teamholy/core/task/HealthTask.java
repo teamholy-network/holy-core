a7a218e18ea2a79663cfce017e1419cd3727b7be
yassino <letzyassinehd@gmail.com>
2024-02-20 00:22:06 +0100

    charin pull

--- holy-core-cloudmodule/src/main/java/de/teamholy/core/task/HealthTask.java
+++ holy-core-cloudmodule/src/main/java/de/teamholy/core/task/HealthTask.java
@@ -23,7 +23,7 @@ public class HealthTask implements Runnable {
     public void run() {
         HashMap<String, Long> pingMap = new HashMap<>(healthService.pingMap);
         for (String server : pingMap.keySet()) {
-            if (System.currentTimeMillis() - healthService.getLastPing(server) <= 7500L) continue;
+            if (System.currentTimeMillis() - healthService.getLastPing(server) <= 20000L) continue;
 
             healthService.removePing(server);
             healthService.stopService(server);
