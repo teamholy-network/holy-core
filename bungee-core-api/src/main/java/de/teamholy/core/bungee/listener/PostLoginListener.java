d98937cfcfea8a3d822e3c7bb481f2d55c41bcdc
Gregor <62483539+gin337@users.noreply.github.com>
2024-03-08 16:22:06 +0100

    t

--- bungee-core-api/src/main/java/de/teamholy/core/bungee/listener/PostLoginListener.java
+++ bungee-core-api/src/main/java/de/teamholy/core/bungee/listener/PostLoginListener.java
@@ -49,7 +49,7 @@ public class PostLoginListener implements Listener {
         proxiedPlayer.sendMessage("          §f§lWELCOME ON §6§lTEAMHOLY          ");
         proxiedPlayer.sendMessage("");
         proxiedPlayer.sendMessage("§3Discord §8» §7https://discord.gg/teamholy");
-        proxiedPlayer.sendMessage("§cStore §8» §7https://shop.teamholy.de");
+        proxiedPlayer.sendMessage("§cStore §8» §7https://teamholy.de/shop");
         proxiedPlayer.sendMessage("§6Vote §8» §7https://teamholy.de/vote");
         proxiedPlayer.sendMessage("§5Website §8» §7https://teamholy.de");
         proxiedPlayer.sendMessage("");
