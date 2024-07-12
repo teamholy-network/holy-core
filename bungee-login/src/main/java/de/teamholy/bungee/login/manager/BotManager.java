fb534c0b1017aa26408815ff408aeee9fa5f6da7
Gregor <62483539+gin337@users.noreply.github.com>
2024-07-12 14:57:33 +0200

    fix

--- bungee-login/src/main/java/de/teamholy/bungee/login/manager/BotManager.java
+++ bungee-login/src/main/java/de/teamholy/bungee/login/manager/BotManager.java
@@ -6,32 +6,32 @@ import java.util.concurrent.TimeUnit;
 
 public class BotManager {
 
-	private static int registers = 0;
+    private static int registers = 0;
 
-	private static int loggins = 0;
-	
-	public static void init() {
-		TaskAPI.runScheduledAtFixedRate(() -> {
-			registers = 0;
-			loggins = 0;
-		}, 10, 10, TimeUnit.MINUTES);
-	}
+    private static int loggins = 0;
 
-	public static boolean isBotProtectionEnableRegister() {
-		return true;
-        //registers >= 5;
-	}
+    public static void init() {
+        TaskAPI.runScheduledAtFixedRate(() -> {
+            registers = 0;
+            loggins = 0;
+        }, 10, 10, TimeUnit.MINUTES);
+    }
 
-	public static boolean isBotProtectionEnableLogin() {
-		return true;
+    public static boolean isBotProtectionEnableRegister() {
+        //return true;
+        return registers >= 30;
+    }
+
+    public static boolean isBotProtectionEnableLogin() {
+        return false;
         //loggins >= 10;
-	}
-	
-	public static void register() {
-		registers += 1;
-	}
-	
-	public static void loggin() {
-		loggins += 1;
-	}
+    }
+
+    public static void register() {
+        registers += 1;
+    }
+
+    public static void loggin() {
+        loggins += 1;
+    }
 }
