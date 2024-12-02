0651e98e57d9f447f04530dffa68c431c3bb2d7a
Noah Leibetseder <Noah@leibetseder.de>
2024-12-02 14:07:48 +0100

    refactor: improve GameProfile reflection handling
    Switched to using standardized logging with `Logger` for better error handling and diagnostics. Refactored field names and added documentation comments for clarity and maintainability. Removed deprecated PacketPlayOutPlayerInfo handling. Added method to retrieve GameProfile from Player object via reflection.
    
    Author: Noah Leibetseder

--- bukkit-markupapi/src/main/java/eu/koboo/markup/util/GameProfileReflection.java
+++ bukkit-markupapi/src/main/java/eu/koboo/markup/util/GameProfileReflection.java
@@ -1,56 +1,98 @@
 package eu.koboo.markup.util;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
-import java.util.List;
+import java.lang.reflect.Method;
 import java.util.UUID;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class GameProfileReflection {
 
-    private static Field NAME_FIELD;
-    private static Field UUID_FIELD;
-    private static Field PLAY_OUT_INFO;
+    /**
+     * A logger for logging messages and exceptions within the GameProfileReflection class.
+     * This Logger is used to provide detailed logging information at various levels of severity.
+     */
+    private static final Logger LOGGER = Logger.getLogger(GameProfileReflection.class.getName());
+
+    /**
+     * A reflection-based representation of the "name" field within the GameProfile class.
+     * This field is used to access and modify the "name" attribute of a GameProfile instance
+     * using reflection, allowing for dynamic manipulation of private field values.
+     */
+    private static Field nameField;
+
+    /**
+     * A reflection-based Field object representing the "id" field of the GameProfile class.
+     * This static field is initialized within a static block by accessing the declared field
+     * "id" of the GameProfile class and making it accessible for reflection-based operations.
+     * It is used to modify the UUID of a GameProfile instance through reflection.
+     */
+    private static Field idField;
 
     static {
         try {
-            NAME_FIELD = GameProfile.class.getDeclaredField("name");
-            NAME_FIELD.setAccessible(true);
+            nameField = GameProfile.class.getDeclaredField("name");
+            nameField.setAccessible(true);
 
-            UUID_FIELD = GameProfile.class.getDeclaredField("id");
-            UUID_FIELD.setAccessible(true);
-
-            PLAY_OUT_INFO = PacketPlayOutPlayerInfo.class.getDeclaredField("b");
-            PLAY_OUT_INFO.setAccessible(true);
+            idField = GameProfile.class.getDeclaredField("id");
+            idField.setAccessible(true);
         } catch (Exception e) {
-            Bukkit.getLogger().severe("Reflection error! Couldn't use NickAPI!");
-            e.printStackTrace();
+            Bukkit.getLogger().severe("Reflection error! Couldn't use GameProfileReflection!");
+            LOGGER.log(Level.SEVERE, "Exception in static initializer block", e);
         }
     }
 
+    /**
+     * Modifies the name of a given GameProfile using reflection.
+     *
+     * @param profile The GameProfile whose name is to be set.
+     * @param name The new name to assign to the GameProfile.
+     */
     public static void setName(GameProfile profile, String name) {
         try {
-            NAME_FIELD.set(profile, name);
+            nameField.set(profile, name);
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed to set name in GameProfile", e);
         }
     }
 
+    /**
+     * Sets the UUID for the specified GameProfile.
+     *
+     * @param profile the GameProfile whose UUID is to be set
+     * @param uuid the new UUID to set in the GameProfile
+     */
     public static void setUUID(GameProfile profile, UUID uuid) {
         try {
-            UUID_FIELD.set(profile, uuid);
+            idField.set(profile, uuid);
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed to set UUID in GameProfile", e);
         }
     }
 
-    public static void setPlayerInfo(PacketPlayOutPlayerInfo packet, List<PacketPlayOutPlayerInfo.PlayerInfoData> list) {
+    /**
+     * Retrieves the GameProfile associated with a given Player object. This method
+     * utilizes reflection to access the private fields of the Player's underlying
+     * entity to obtain the GameProfile.
+     *
+     * @param player The Player whose GameProfile is to be retrieved.
+     * @return The GameProfile of the given Player or null if the operation fails.
+     */
+    public static GameProfile getGameProfile(Player player) {
         try {
-            PLAY_OUT_INFO.set(packet, list);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            Method getHandle = player.getClass().getMethod("getHandle");
+            Object entityPlayer = getHandle.invoke(player);
+            Field profileField = entityPlayer.getClass().getDeclaredField("gameProfile");
+            profileField.setAccessible(true);
+            return (GameProfile) profileField.get(entityPlayer);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Failed to get GameProfile", e);
+            return null;
         }
     }
 }
