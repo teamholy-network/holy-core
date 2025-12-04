f6ab36b4b6f6ef12b7c645a4ef0584f6397936db
JavaExceptionDE <64277088+javaexceptionde@users.noreply.github.com>
2025-12-04 07:05:03 +0100

    fix: StopCommand: Use AtomicInteger for countdown management
    Replaced the mutable integer array used for managing the server restart countdown in `StopCommand` with `AtomicInteger`. This ensures thread-safety and clarity in the countdown logic. Additionally, refactored the `BukkitRunnable` code for better readability and maintainability. These improvements enhance code reliability and align with modern coding practices.
    
    Author: Jonathan Bull

--- bukkit-core-api/src/main/java/de/teamholy/core/bukkit/commands/StopCommand.java
+++ bukkit-core-api/src/main/java/de/teamholy/core/bukkit/commands/StopCommand.java
@@ -2,6 +2,7 @@ package de.teamholy.core.bukkit.commands;
 
 import de.skydb.translateapi.bindings.BukkitTranslateAPI;
 import de.teamholy.core.bukkit.BukkitCore;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -14,33 +15,37 @@ public class StopCommand implements CommandExecutor {
 
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!commandSender.hasPermission("teamholy.stop")) return false;
+    public boolean onCommand(CommandSender commandSender, Command command, String s,
+        String[] strings) {
+        if (!commandSender.hasPermission("teamholy.stop")) {
+            return false;
+        }
 
         BukkitCore.RESTART = true;
 
-        final int[] i = {5};
-
+        final AtomicInteger countDown = new AtomicInteger(5);
         new BukkitRunnable() {
             @Override
             public void run() {
-                if (i[0] == 0) {
-                    Bukkit.getOnlinePlayers().forEach(player1 -> player1.kickPlayer("§c"+BukkitTranslateAPI.translate(player1,"Server restart")));
-                    Bukkit.getScheduler().runTaskLater(BukkitCore.getInstance(), Bukkit::shutdown, 20L);
+                final int currentCountDown = countDown.getAndDecrement();
+                if (currentCountDown == 0) {
+                    Bukkit.getOnlinePlayers().forEach(player1 -> player1.kickPlayer(
+                        "§c" + BukkitTranslateAPI.translate(player1, "Server restart")));
+                    Bukkit.getScheduler()
+                        .runTaskLater(BukkitCore.getInstance(), Bukkit::shutdown, 20L);
+                    cancel();
                 }
 
                 Bukkit.getOnlinePlayers().forEach(player1 -> {
                     player1.sendMessage("");
-                    player1.sendMessage("§c"+ BukkitTranslateAPI.translate(player1,"Server restarts in")+" §l" + i[0] + " §c"+BukkitTranslateAPI.translate(player1,"seconds"));
+                    player1.sendMessage("§c" + BukkitTranslateAPI.translate(player1, "Server " + "restarts in")
+                            + " §l" + currentCountDown + " §c" + BukkitTranslateAPI.translate(
+                            player1, "seconds"));
                     player1.sendMessage("");
                     player1.playSound(player1.getLocation(), Sound.NOTE_BASS, 20, 20);
                 });
-
-                i[0]--;
             }
         }.runTaskTimer(BukkitCore.getInstance(), 0, 20);
-
-
 
         return false;
     }
